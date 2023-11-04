dba9611665a64513aa86d2b07b8673e0d3eddbc8
thinhtran1110 <thinh.tran1110@hcmut.edu.vn>
2023-11-04 11:30:20 +0700

    thêm api tạo account (chua xong)

--- src/Authentication/auth.controller.ts
+++ src/Authentication/auth.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  // @Get()
+  // getHello(): string {
+  //   return this.authService.getHello();
+  // }
+
+  @Post('register')
+  async createAccount(
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.createAccount(username, password);
   }
 }
--- src/Authentication/auth.service.ts
+++ src/Authentication/auth.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Role } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 import { DatabaseService } from 'libs/database.module';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  async login(username: string, password: string): string {
-    const user = await this.databaseService.findUser(username);
+  async createAccount(username: string, password: string): Promise<string> {
+    let salt = await bcrypt.genSalt(10);
+    let passwordHash = await bcrypt.hash(password, salt);
+    let user = await this.databaseService.account.create({
+      data: {
+        username: username,
+        password: passwordHash,
+        role: Role.STAFF,
+      },
+    });
+    return user.username;
   }
 }
