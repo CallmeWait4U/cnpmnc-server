c74bb254cf6bbf8a1a78603c3322508e79e7fec3
thinhtran1110 <thinh.tran1110@hcmut.edu.vn>
2023-11-04 10:51:43 +0700

    .

--- src/Authentication/auth.module.ts
+++ src/Authentication/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'libs/database.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/Authentication/auth.service.ts
+++ src/Authentication/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'libs/database.module';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async login(username: string, password: string): string {
+    const user = await this.databaseService.findUser(username);
   }
 }
