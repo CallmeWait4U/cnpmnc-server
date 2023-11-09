33522a293148fd50f55931308aa8f10b165d5f44
kuro <nkb.gdev@gmail.com>
2023-11-09 11:18:19 +0700

    move to module account

--- src/Account/changepass.module.ts
+++ src/Account/changepass.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'libs/database.module';
-import { ChangepassController } from './changepass.controller';
 import { ChangepassService } from './changepass.service';
+import { ChangepassController } from './changepass.controller';
 
 @Module({
   imports: [DatabaseModule],
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'libs/database.module';
 import { AccountModule } from './Account/account.module';
+import { ChangepassModule } from './Account/changepass.module';
 import { AuthModule } from './Authentication/auth.module';
 import { StaffModule } from './Staff/staff.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChangepassModule } from './Authentication/changepass.module';
 
 @Module({
   imports: [DatabaseModule, AuthModule, StaffModule, AccountModule, ChangepassModule],
