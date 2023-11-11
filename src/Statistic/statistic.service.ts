dad457509beb914c0576b95452ec0814f159f48d
thinhtran1110 <thinh.tran1110@hcmut.edu.vn>
2023-11-11 10:44:51 +0700

    add month

--- src/Statistic/statistic.service.ts
+++ src/Statistic/statistic.service.ts
@@ -5,10 +5,20 @@ import { DatabaseService } from 'libs/database.module';
 export class StatisticService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async getStatusStatistics() {
+  async getStatusStatistics(month) {
+    const dateNow = new Date();
+    const dateAgo = new Date();
+    dateAgo.setUTCMonth(dateNow.getUTCMonth() - month);
+    // console.log(dateAgo, dateNow);
     const statusStatistics = await this.databaseService.request.groupBy({
       by: ['status'],
       _count: { status: true },
+      where: {
+        startDate: {
+          gte: dateAgo,
+          lt: dateNow,
+        },
+      },
     });
     const returnStatistics = statusStatistics.map((status) => {
       return {
