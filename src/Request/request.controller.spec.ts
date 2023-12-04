4ab3604c14416c757e0cffacaed5dc9f9f4659d1
trickstars <nguyenminhkhoe123@gmail.com>
2023-12-04 16:30:08 +0700

    create request testing

--- src/Request/request.controller.spec.ts
+++ src/Request/request.controller.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RequestController } from './request.controller';
+import { RequestService } from './request.service';
+import { AuthGuard } from '../Authentication/auth.guard';
+import { CreateRequestDTO } from './dtos/create.request.dto';
+import { JwtService } from '@nestjs/jwt';
+
+jest.mock('./request.service');
 
 describe('RequestController', () => {
-  let controller: RequestController;
+  let requestController: RequestController;
+  let requestService: RequestService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RequestController],
+      providers: [
+        RequestService,
+        AuthGuard,
+        {
+          provide: JwtService,
+          useValue: {
+            //
+          },
+        },
+      ]
     }).compile();
 
-    controller = module.get<RequestController>(RequestController);
+    requestController = module.get<RequestController>(RequestController);
+    requestService = module.get<RequestService>(RequestService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(requestController).toBeDefined();
   });
+
+  describe('createRequest', () => {
+    it('it should return success message', async () => {
+      const mockReq: CreateRequestDTO = {
+        id: 'StaffId',
+        reason: 'Some reason',
+        startDate: 'startDate',
+        endDate: 'endDate'
+      }
+
+      jest.spyOn(requestService, 'createRequest').mockResolvedValueOnce({message: 'SUCCESS'});
+
+      expect(await requestService.createRequest(mockReq)).toStrictEqual({message: "SUCCESS"})
+    })
+  })
 });
